fd788a72f30711fa711fe1fe8c94b17b5dd6ba2f
AveryMunoz <averymunoz10@gmail.com>
2026-04-10 12:30:35 -0400

    Initial Commit

---
+++ Producer.py
@@ -0,0 +1,14 @@
+# Task Processing System, incorporating different data structure skills for practice
+
+
+while True:
+    User_Task= input("Enter a task to add to the queue: ")
+
+    if User_Task.strip() == "": # .strip to account for random space inputs
+        print("No task entered. Please enter a valid task.")    
+    elif User_Task.strip() == "exit" or User_Task.strip() == "Exit":
+        print("Exiting the task processing system.")
+        break
+    else:
+        with open("queue.txt", "a") as file:
+            file.write(User_Task + "\n")
